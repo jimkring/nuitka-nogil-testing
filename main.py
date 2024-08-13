14d244faf78a5410e3c075f7dfafb141ccc5541b
Jim Kring <jim.kring@jki.net>
2024-08-12 19:42:24 -0700

    better testing in source and exe

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world!")
+import sys
+import sysconfig
+
+gill_enabled = sys._is_gil_enabled()
+Py_GIL_DISABLED = sysconfig.get_config_var("Py_GIL_DISABLED")
+
+print(f"sys._is_gil_enabled(): {gill_enabled}")
+print(f"sysconfig.get_config_var(\"Py_GIL_DISABLED\"): {Py_GIL_DISABLED}")
